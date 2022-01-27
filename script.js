2e564c7c9bab7fc376f3b7c68455befe79dc25f4
MANAN1352 <98357043+MANAN1352@users.noreply.github.com>
2022-01-27 13:46:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function compute()
-{
-    principal = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
-    result = document.getElementById("result");
-    result.innerHTML="the Resut is:" +(principal * years * rate / 100);
-}
